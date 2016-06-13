d14ae246e4d18b5b3109cfb7f3cc47f1032e4d26
Lucas Döring <lucasdoering@Lucas-Macbook.local>
2016-06-13 11:56:15 +0200

    TimeButon add

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -4,14 +4,26 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
-	private @FXML Button tooManyButton;
-	
-	public void tooManyButtonAction() {
-		new Alert(AlertType.ERROR, "Too Many Buttons!").showAndWait();
-	}
+    @FXML
+    private Button tooManyButton;
+    @FXML
+    private Label timeLabel;
+
+    public void tooManyButtonAction() {
+        new Alert(AlertType.ERROR, "Too Many Buttons!").showAndWait();
+    }
+
+
+    @FXML
+    public void showTime() {
+        timeLabel.setText(LocalDateTime.now().toLocalTime().toString());
+    }
 }
