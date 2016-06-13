f59edf4bc9dec7c830fc81204306d79fb980ec1c
dgelessus <dgelessus@users.noreply.github.com>
2016-06-13 11:37:53 +0200

    Add Too Many Buttons button

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,17 @@
 package de.hhu.propra16;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+	private @FXML Button tooManyButton;
+	
+	public void tooManyButtonAction() {
+		new Alert(AlertType.ERROR, "Too Many Buttons!").showAndWait();
+	}
 }
